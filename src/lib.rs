b8ce8b734055a8dcb13d45a827d4d3bde6a2008e
rulewrite <rulewrite@gmail.com>
2022-09-11 17:21:37 +0900

    feat: 스레드 풀 개수를 0으로 지정했을 경우 밸리데이션 및 문서화

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// 새 스레드풀을 생성
+    ///
+    /// size는 풀의 스레드 수
+    ///
+    /// # Panics
+    ///
+    /// size가 0이면 `new` 함수는 패닉을 일으킨다.
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         ThreadPool
     }
 
