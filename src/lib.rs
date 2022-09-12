df49fbbe7130c5b1f0e1c3cdc7fc8ff22484f45b
rulewrite <rulewrite@gmail.com>
2022-09-13 03:42:36 +0900

    fix: 비동기 처리되지 않는 이슈
    https://doc.rust-lang.org/book/ch20-02-multithreaded.html#implementing-the-execute-method
    마지막 부분 참고

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,9 @@ struct Worker {
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            match receiver.lock().unwrap().recv() {
+            let message = receiver.lock().unwrap().recv();
+
+            match message {
                 Ok(job) => {
                     println!("Worker {id} got a job; executing.");
 
